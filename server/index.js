33bdbacef369a0fc67a469d3ce60f730b65518a3
Masaki Nabeshima <mnabeshimase@gmail.com>
2017-10-02 12:04:28 -0700

    Set up webpack and babel for React

---
+++ server/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+app.use(express.static('dist'));
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
