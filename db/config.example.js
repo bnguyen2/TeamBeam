1fdfc30868787f6b7d52db68dba53abfad67406f
Masaki Nabeshima <mnabeshimase@gmail.com>
2017-10-03 12:15:58 -0700

    Define schema using bookshelf

---
+++ db/config.example.js
@@ -0,0 +1,10 @@
+module.exports = {
+  client: 'pg',
+  version: '7.6',
+  connection: {
+    host : '127.0.0.1',
+    user : 'your_database_user',
+    password : 'your_database_password',
+    database : 'myapp_test'
+  }
+};
