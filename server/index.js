666491bae79ba4159fd5fa86e0c06bb6d16c6f3c
Evan Chen <evanchen7@yahoo.com>
2017-10-04 10:23:33 -0700

    'partial passport implementation'

--- server/index.js
+++ server/index.js
@@ -3,12 +3,20 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const bodyParser = require('body-parser');
 const routes = require('./routes');
+const passport = require('passport');
+const session = require('express-session');
 
 
 app.use(express.static('public'));
 app.use(express.static('dist'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// Passport Middleware
+app.use(session({ secret: 'keyboard cat',resave: true, saveUninitialized:true})); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+
 
 // Connect all routes to app
 app.use('/', routes)
