9df9e84a57bce3a4e38f2daf507e54702b8bbdef
Masaki Nabeshima <mnabeshimase@gmail.com>
2017-10-05 11:33:34 -0700

    Link profile page and forum page

--- client/index.jsx
+++ client/index.jsx
@@ -1,6 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, hashHistory } from 'react-router-dom'
 import Musician from '../client/Musician.jsx'
 import Composer from '../client/Composer.jsx'
+import Login from './Login.jsx'
+import Forum from './Forum.jsx'
+import Navigation from './Navigation.jsx'
 
-ReactDOM.render(<Composer/>, document.getElementById('app'));
+ReactDOM.render((
+  <BrowserRouter>
+    <Navigation>
+      <Route path="/" exact={true} component={Composer} />
+      <Route path="/forum" component={Forum} />
+    </Navigation>
+  </BrowserRouter>
+), document.getElementById('app'));
