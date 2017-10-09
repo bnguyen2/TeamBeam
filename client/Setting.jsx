cf519eb7bb5b4874f9d845fccff73c8de3ae6e17
Evan Chen <evanchen7@yahoo.com>
2017-10-08 17:07:00 -0700

    'setting page created'

---
+++ client/Setting.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Modal from 'react-modal';
+const Axios = require('axios');
+
+export default class Setting extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      
+    }
+  }
+
+  componentDidMount() {
+
+  }
+
+  render() {
+    return(
+      <div>
+      </div>
+    );
+  }
+}
